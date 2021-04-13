3fd237ce8a865b964d105c45cce2762dc360c907
Daniel Brandao <dannnbrandao@gmail.com>
2021-04-13 01:04:00 -0300

    Criado layout, colocando uma barra lateral com botões

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -18,33 +18,70 @@ class _HomePageState extends State<HomePage> {
       ),
       // !
 
-      body: Container(
-        alignment: Alignment.center,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            // ? INfo
-            Text('Contador'),
-            Text('10'),
-          ],
-        ),
+      body: Row(
+        // mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Expanded(
+            flex: 3,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Text('Testando'),
+                Text('Testando'),
+              ],
+            ),
+          ),
+          Expanded(
+            flex: 1,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  height: 300,
+                  width: 75, // ! Verificar caso precise remover
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(30),
+                    color: Colors.blue,
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.grey.withOpacity(.5),
+                        blurRadius: 2,
+                        offset: Offset(4, 5),
+                      ),
+                    ],
+                  ),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      FloatActionButton(
+                        icon: Icons.add,
+                        onPressed: () {},
+                      ),
+                      FloatActionButton(
+                        icon: Icons.remove,
+                        onPressed: () {},
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
+
+      // ? Action Button
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.refresh),
+        splashColor: Colors.red,
+        onPressed: () {},
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+
+      // ? Navigator bar
       bottomNavigationBar: Container(
-        height: 100,
+        height: 80,
         color: Colors.blue,
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            FloatActionButton(
-              icon: Icons.add,
-              onPressed: () {},
-            ),
-            FloatActionButton(
-              icon: Icons.remove,
-              onPressed: () {},
-            ),
-          ],
-        ),
       ),
     );
   }
